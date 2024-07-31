cee8f15118d61b021e1be9e3a5827942185e7295
Aleksa123123 <107876697+Aleksa123123@users.noreply.github.com>
2024-07-31 17:39:44 +0200

    add login doctor & read file

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -17,7 +17,7 @@ class Program
         string adminUsername = "admin";
         string adminPassword = "123456";
 
-        string doctorUsername = "doctor";
+        string doctorUsername = "Doktor Goran";
         string doctorPassword = "123456";
 
         int cursorLeft = Console.CursorLeft;
@@ -25,8 +25,9 @@ class Program
 
         while (trueS)
         {
-
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.Write("Your choice: ");
+            Console.ForegroundColor = ConsoleColor.White;
 
             loginAs = Console.ReadLine();
 
@@ -84,7 +85,7 @@ class Program
                     Console.SetCursorPosition(0, cursorTop);
                 }
             }
-
+            Console.WriteLine("");
             trueS = true;
             Console.WriteLine("Please type password");
             Console.WriteLine("");
@@ -122,6 +123,7 @@ class Program
             string yes = "yes";
             string no = "no";
             string yesNo = Console.ReadLine();
+            Console.Clear();
             if (yesNo == yes)
             {
                 // Prompt the user for the doctor's name
@@ -145,7 +147,6 @@ class Program
                 Directory.CreateDirectory(folderPath);
                 Console.WriteLine($"Folder '{sanitizedFolderName}' created successfully.");
 
-
                 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -153,6 +154,7 @@ class Program
                 // Prompt the user for details to be written in the text file
                 Console.WriteLine("Enter doctor speciality:");
                 string doctorDetails = Console.ReadLine();
+                string pacijenti = "pacijenti.txt";
 
                 // Define the path for the text file within the new folder
                 string filePath = Path.Combine(folderPath, doctorDetails+".txt");
@@ -163,6 +165,10 @@ class Program
                     writer.WriteLine(doctorDetails);
                 }
 
+                filePath = Path.Combine(folderPath, pacijenti);
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                }
                 Console.WriteLine($"Text file 'details.txt' created in folder '{sanitizedFolderName}' with the provided details.");
             }
             else if (yesNo == no)
@@ -170,8 +176,101 @@ class Program
                 Console.WriteLine("oke");
             }
         }
-        
 
+        if (loginAs == doctorLogin)
+        {
+            trueS = true;
+            Console.WriteLine("Please type username");
+            Console.WriteLine("");
+            while (trueS)
+            {
+                Console.Write("Username: ");
+                cursorLeft = Console.CursorLeft;
+                cursorTop = Console.CursorTop;
+
+                username = Console.ReadLine();
+
+                if (username == doctorUsername)
+                {
+                    trueS = false;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, cursorTop - 1);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, cursorTop - 1);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please try again:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.SetCursorPosition(0, cursorTop);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, cursorTop);
+                }
+            }
+            Console.WriteLine("");
+            trueS = true;
+            Console.WriteLine("Please type password");
+            Console.WriteLine("");
+            while (trueS)
+            {
+                Console.Write("Password: ");
+                cursorLeft = Console.CursorLeft;
+                cursorTop = Console.CursorTop;
+
+                password = Console.ReadLine();
+
+                if (password == doctorPassword)
+                {
+                    trueS = false;
+                }
+                else
+                {
+                    Console.SetCursorPosition(0, cursorTop - 1);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, cursorTop - 1);
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please try again:");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.SetCursorPosition(0, cursorTop);
+                    Console.Write(new string(' ', Console.WindowWidth));
+                    Console.SetCursorPosition(0, cursorTop);
+                }
+            }
+
+            Console.Clear();
+
+            //
+
+            if (username == "Doktor Goran")
+            {
+                string doctorFolderPath = Path.Combine("Doktor Goran");
+
+                // Check if the folder exists
+                if (Directory.Exists(doctorFolderPath))
+                {
+                    Console.WriteLine($"Accessing folder: {doctorFolderPath}");
+                    Console.WriteLine("");
+
+                    // List files or handle the folder contents
+                    string[] files = Directory.GetFiles(doctorFolderPath);
+                    Console.WriteLine("Files in the folder:");
+                    foreach (string file in files)
+                    {
+                        Console.WriteLine(Path.GetFileName(file));
+                    }
+
+                    // You can add code here to handle files or interact with them
+                }
+                else
+                {
+                    Console.WriteLine("Doctor's folder does not exist.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid username.");
+            }
+        }
 
 
 
@@ -179,5 +278,6 @@ class Program
 
         
     }
+
 }
 
